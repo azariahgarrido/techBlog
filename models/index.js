f87fd5f38846a06174b968528a181b51f4ca5ac4
azariahgarrido <azariahdanielgarrido@gmail.com>
2022-09-10 09:42:26 -0700

    capitalization

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Post = require('./post');
+const Comment = require('./comment');
 
 Post.belongsTo(User, {
   foreignKey: 'userId',
